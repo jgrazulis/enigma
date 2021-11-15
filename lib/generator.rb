4832b19caffb9d27ddd8a1b028d337e5bd07fcfa
Jessica Grazulis <jlgrazulis@gmail.com>
2021-11-15 13:21:21 -0700

    Add tests for shifts and offsets using defaults

--- lib/generator.rb
+++ lib/generator.rb
@@ -7,22 +7,95 @@ class Generator
   end
 
   def random_key
-    key = []
-    key << rand(00000..9999)
+    # key = []
+    # key <<
+    key_num = rand(00000..9999)
+    num_string = key_num.to_s
+    # delete("[]")
+    num_string.rjust(5, '0')
   end
 
   def key_pad(key_num)
     num_string = key_num.to_s.delete("[] ,")
     num_string.rjust(5, '0')
   end
+    # key_pad(random_key)
 
-  def date_string(date = Date.today)
+  def a_key
+    a = (random_key[0]) + (random_key[1])
+    # a.to_i
+  end
+
+  def b_key
+    b = (random_key[1]) + (random_key[2])
+    b.to_i
+  end
+
+
+  def c_key
+    c= (random_key[2]) + (random_key[3])
+    c.to_i
+  end
+
+  def d_key
+    d= (random_key[3]) + (random_key[4])
+    d.to_i
+  end
+
+  def date_string(date = Date.today) # date should be in format of DDMMYY
     date = Date.today if date == nil
     date.to_s
   end
 
-  def offset
-    date_string
+  def sq_rt_date
+    # date_parse = Date.parse(date_string)
+    # new_date = a.to_time.to_i
+    Integer.sqrt(new_date)
   end
 
+  def date_parse
+    # a = Date.parse(date_string)
+    Date.parse(date_string)
+  end
+
+  def new_date
+    # new_date = a.to_time.to_i
+    date_parse.to_time.to_i
+  end
+
+  def date_array
+    new_date.to_s.split("")
+  end
+
+
+  def last_four
+  # take the epoch (new_date) value last 4 digits by
+    date_array[date_array.length - 4] + date_array[date_array.length - 3] + date_array[date_array.length - 2] +  date_array[date_array.length - 1]
+  end
+
+  # split into different shift (a, b, c, d) offsets by
+  # last_four[0] etc. to split up for individual offsets
+  def a_offset
+    last_four[0].to_i
+  end
+
+  def b_offset
+    last_four[1].to_i
+  end
+
+  def c_offset
+    last_four[2].to_i
+  end
+
+  def d_offset
+    last_four[3].to_i
+  end
+
+  def shifts
+    shifts = Hash.new(0)
+    {b: (b_offset + b_key),
+    c: (c_offset + c_key),
+    d: (d_offset + d_key)}
+
+  end
 end
--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -17,9 +17,9 @@ describe Generator do
 
   it 'generates #random_key' do
     generator = Generator.new
-    expect(generator.random_key).to be_an(Array)
-    expect(generator.random_key.first).to be_an(Integer)
-    expect(generator.random_key.last).to be_an(Integer)
+    expect(generator.random_key).to be_an(String)
+    # expect(generator.random_key.first).to be_an(Integer)
+    # expect(generator.random_key.last).to be_an(Integer)
   end
 
   it 'pads keys with 0s' do
@@ -29,16 +29,63 @@ describe Generator do
     expect(generator.key_pad([0, 0, 0, 0])).to eq("00000")
   end
 
+  it 'has keys' do
+    generator = Generator.new
+    expect(generator.a_key).to be_an(String)
+    expect(generator.b_key).to be_an(Integer)
+    expect(generator.c_key).to be_an(Integer)
+    expect(generator.d_key).to be_an(Integer)
+  end
+
   it 'returns the date as a string' do
     generator = Generator.new
     expect(generator.date_string).to be_a(String)
   end
 
-  it 'generates offsets' do
+  it '#date_parse' do
     generator = Generator.new
-    expect(generator.offset).to be_an(String)
+    date = Date.today
+    expect(generator.date_parse).to eq(date)
   end
 
+  it '#new_date' do
+    generator = Generator.new
+    expect(generator.new_date).to be_an(Integer)
+  end
 
+  it 'returns #sq_rt_date' do
+    generator = Generator.new
+    expect(generator.sq_rt_date).to be_an(Integer)
+  end
+
+  it 'date_array' do
+    generator = Generator.new
+    expect(generator.date_array).to be_an(Array)
+  end
+
+  it 'splits out the last four digits of the date array' do
+    generator = Generator.new
+    expect(generator.last_four).to be_a(String)
+  end
+
+  it 'generates offsets' do
+    generator = Generator.new
+    expect(generator.a_offset).to be_an(Integer)
+    expect(generator.b_offset).to be_an(Integer)
+    expect(generator.c_offset).to be_an(Integer)
+    expect(generator.d_offset).to be_an(Integer)
+  end
+
+  it 'can create shifts with a default key and date' do
+    generator = Generator.new
+    expect(generator.shifts).to be_a(Hash)
+  end
+
+  it 'can create shifts with a given date and key' do
+    generator = Generator.new
+    key = "02715"
+    offsets = "040895"
+    expect(generator.shifts).to eq({a: 3, b: 27, c: 73, d: 20})
+  end
 
 end
